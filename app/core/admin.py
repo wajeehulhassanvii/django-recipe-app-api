d1e3a3175c903ddae95a0f5f10b6cc9b04701865
Wajeeh Ul Hassan <wajeehulhassan_vii@hotmail.com>
2020-06-14 00:47:54 +1000

    ingredients api integrated

--- app/core/admin.py
+++ app/core/admin.py
@@ -37,3 +37,4 @@ class UserAdmin(BaseUserAdmin):
 # it will register model.Users to UserAdmin
 admin.site.register(models.User, UserAdmin)
 admin.site.register(models.Tag)
+admin.site.register(models.Ingredient)
--- app/recipe/views.py
+++ app/recipe/views.py
@@ -3,6 +3,7 @@ from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 
 from core.models import Tag
+from core.models import Ingredient
 
 from recipe import serializers
 
@@ -22,4 +23,22 @@ class TagViewSet(viewsets.GenericViewSet,
 
     def perform_create(self, serializer):
         """create a new tag"""
+        serializer.save(user=self.request.user)
+
+
+class IngredientViewSet(viewsets.GenericViewSet,
+                        mixins.ListModelMixin,
+                        mixins.CreateModelMixin):
+    """Manage ingredients in the database"""
+    authentication_classes = (TokenAuthentication, )
+    permission_classes = (IsAuthenticated, )
+    queryset = Ingredient.objects.all()
+    serializer_class = serializers.IngredientSerializer
+
+    def get_queryset(self):
+        """Return objects for the current authenticated user"""
+        return self.queryset.filter(user=self.request.user).order_by('-name')
+
+    def perform_create(self, serializer):
+        """create a new ingredient"""
         serializer.save(user=self.request.user)
