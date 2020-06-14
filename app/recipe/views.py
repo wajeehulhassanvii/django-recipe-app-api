30e355a32eb2403366277d5381daf0ce0e6e8824
Wajeeh Ul Hassan <wajeehulhassan_vii@hotmail.com>
2020-06-14 21:42:12 +1000

    recipe ingredient fixed and refactored

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -2,8 +2,7 @@ from rest_framework import viewsets, mixins
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 
-from core.models import Tag
-from core.models import Ingredient
+from core.models import Tag, Ingredient
 
 from recipe import serializers
 
@@ -12,20 +11,20 @@ class BaseRecipeAttrViewSet(viewsets.GenericViewSet,
                             mixins.ListModelMixin,
                             mixins.CreateModelMixin):
     """Base viewset for user owned recipe attributes"""
-    authentication_classes = (TokenAuthentication, )
-    premission_classes = (IsAuthenticated, )
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
 
     def get_queryset(self):
         """Return objects for the current authenticated user only"""
         return self.queryset.filter(user=self.request.user).order_by('-name')
 
     def perform_create(self, serializer):
-        """Create a new object"""
+        """Create a new ingredient"""
         serializer.save(user=self.request.user)
 
 
 class TagViewSet(BaseRecipeAttrViewSet):
-    """Manage tags in the database """
+    """Manage tags in the database"""
     queryset = Tag.objects.all()
     serializer_class = serializers.TagSerializer
 
