00d3c0961ec73cc8aa3c383a587380e74418f663
Wajeeh Ul Hassan <wajeehulhassan_vii@hotmail.com>
2020-06-15 21:16:15 +1000

    Create recipe is working

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -44,3 +44,8 @@ class RecipeSerializer(serializers.ModelSerializer):
         )
         read_only_fields = ('id',)
 
+
+class RecipeDetailSerializer(RecipeSerializer):
+    """Serializer a recipe detail"""
+    ingredients = IngredientSerializer(many=True, read_only=True)
+    tags = TagSerializer(many=True, read_only=True)
